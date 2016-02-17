c828c8db1cb407b97b7f2ec9cbfab9426671c346
amit4alljava@gmail.com <amit4alljava@gmail.com>
2016-02-17 20:44:19 +0530

    Server Call

---
+++ Angular/ServiceVsFactory/dependentmodule/factory.js
@@ -0,0 +1,13 @@
+app.factory("MyService",function(){
+    return {"show":function(){
+        return "Hello App1 Module ";
+    }}
+});
+app.service("MyAppService",function(){
+    this.show=function(){
+        return "HelloApp Service Module";
+    }
+    return {"show":function(){
+        return "Hello App1 Module ";
+    }}
+});
