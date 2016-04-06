15c0175d73b681518e988b0ccbd9f1cd7fc03eff
amit.srivastava4all@gmail.com <amit.srivastava4all@gmail.com>
2016-04-06 13:05:01 +0530

    Codes

---
+++ Node/UsingEvents.js
@@ -0,0 +1,66 @@
+var events = require("events");
+var eventEmitterObject = new events.EventEmitter();
+
+function callback(value){
+    console.log("Ring  Ring By "+value);
+}
+
+// Define
+eventEmitterObject.addListener("ring",callback); // Here Register the Event
+
+
+// Define
+/*eventEmitterObject.on("ring",function(){
+    console.log("Bell Rings");
+});*/
+eventEmitterObject.emit("ring","Amit");  // Event Call
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+function mycallback(value){
+console.log("My Event Called... ",value);
+}
+
+function anothercallback(){
+console.log("Another Call Back Called... ");
+}
+
+
+// this code will define the event
+// the default limit to attach a callback is 10
+ee.setMaxListeners(20);
+ee.on("myevent",mycallback);
+ee.on("myevent",mycallback);
+ee.on("myevent",mycallback);
+ee.on("myevent",mycallback);
+ee.on("myevent",mycallback);
+ee.on("myevent",mycallback);
+ee.on("myevent",mycallback);
+ee.on("myevent",mycallback);
+ee.on("myevent",mycallback);
+ee.on("myevent",mycallback);
+ee.on("myevent",mycallback);
+
+//ee.on("myevent",anothercallback);
+
+ee.emit("myevent",'amit');  // this code trigger the event
+ee.removeListener("myevent",mycallback);
+ee.emit("myevent",'mike');
+ee.emit("myevent",'tim');
+ee.emit("myevent",'tom');*/
